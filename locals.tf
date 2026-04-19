36e27cc135ed9af07801924e1e056a2520563959
pavel-saladukha <pavel.soloduha@outlook.com>
2026-04-19 15:14:52 +0200

    Update docker compose

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
 
   # Pin versions for EC2 bootstrap (init.sh) and templated artifacts (docker-compose.yml).
   software_versions = {
-    docker_compose_cli    = "v2.29.1"
+    docker_compose_cli    = "v5.1.3"
     fail2ban_git_ref      = "1.1.0"
     vaultwarden_image_tag = "1.32.0"
   }
